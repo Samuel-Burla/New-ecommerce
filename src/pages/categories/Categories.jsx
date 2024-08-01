b85d50fbacd0e7e0d1c10fe16f0f51c76f963c4e
Samuel Burla <samuelburla4@gmail.com>
2024-08-01 20:39:50 +0200

    categories - crud operations

--- src/pages/categories/Categories.jsx
+++ src/pages/categories/Categories.jsx
@@ -1,86 +1,147 @@
-// import React from 'react';
-import { useEffect, useState } from "react";
-import "./categories.css";
+import { useEffect } from "react";
+import { useState } from "react";
+import { db } from "../../firebaseConfig";
 import {
   addDoc,
   collection,
   deleteDoc,
   doc,
   getDocs,
+  updateDoc,
+  // updateDoc,
 } from "firebase/firestore";
-import { db } from "../../firebaseConfig";
+import "./categories.css";
 
-const Category = ({ id, title, text }) => {
-  // const id = 1;
-  // const urlQuery = new URLSearchParams(id).toString();
+// const Category = ({ id, title, text }) => {
+//   // const id = 1;
+//   // const urlQuery = new URLSearchParams(id).toString();
 
-  const deleteCategory = async (id) => {
-    const categoryDoc = doc(db, "categories", id);
-    await deleteDoc(categoryDoc);
-  };
-  return (
-    <a href={`/products?${id}`} className="aCategories">
-      <div className="category">
-        <h3>{title}</h3>
-        <p>{text}</p>
-        <button type="button" onClick={()=>{deleteCategory(category.id)}}>
-          Delete Category
-        </button>
-      </div>
-    </a>
-  );
-};
+//   return (
+//     <>
+//       <div className="category">
+//         <a href={`/products?${id}`} className="aCategories">
+//           <h3>{title}</h3>
+//           <p>{text}</p>
+//         </a>
+//         <button
+//           type="button"
+//           onClick={() => {
+//             deleteCategory(id);
+//           }}
+//         >
+//           Delete Category
+//         </button>
+//       </div>
+//     </>
+//   );
+// };
+
 const Categories = () => {
-  const [inputTextCategory, setInputTextCategory] = useState("");
-  const [inputTitleCategory, setInputTitleCategory] = useState("");
+  const [editTitleInput, setEditTitleInput] = useState("");
+  const [editTextInput, setEditTextInput] = useState("");
+
+  const [addTitleInput, setAddTitleInput] = useState("");
+  const [addTextInput, setAddTextInput] = useState("");
+
   const [categories, setCategories] = useState([]);
+  const categoriesRef = collection(db, "categories");
 
-  const categoriesCollection = collection(db, "categories");
-
-  const createCategory = async () => {
-    await addDoc(categoriesCollection, {
-      text: inputTextCategory,
-      title: inputTitleCategory,
-    });
+  //The "CREATE" function
+  const addCategory = async () => {
+    await addDoc(categoriesRef, { title: addTitleInput, text: addTextInput });
   };
 
+  //the useEffect hook used for READING the categories from the database
   useEffect(() => {
+    //The "READ" function
     const getCategories = async () => {
-      const dataCategories = await getDocs(categoriesCollection);
+      const categoriesData = await getDocs(categoriesRef);
       setCategories(
-        dataCategories.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+        categoriesData.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
       );
     };
     getCategories();
   }, []);
 
+  //The "UPDATE" function
+  const editCategory = async (id) => {
+    const categoryDocRef = doc(db, "categories", id);
+    await updateDoc(categoryDocRef, {
+      title: editTitleInput,
+      text: editTextInput,
+    });
+  };
+
+  //The "DELETE" function
+  const deleteCategory = async (id) => {
+    const categoryDocRef = doc(db, "categories", id);
+    await deleteDoc(categoryDocRef);
+  };
+
   return (
     <>
-      <div>
+      <div className="create-form">
         <input
           type="text"
           placeholder="Text"
           onChange={(event) => {
-            setInputTextCategory(event.target.value);
+            setAddTextInput(event.target.value);
           }}
         />
         <input
           type="text"
           placeholder="Title"
           onChange={(event) => {
-            setInputTitleCategory(event.target.value);
+            setAddTitleInput(event.target.value);
           }}
         />
-        <button onClick={createCategory}>Create a category</button>
+        <button onClick={addCategory}>Create a category</button>
       </div>
       <div className="galleryContainer">
         <div className="gallery">
           {categories.map((category) => (
-            <Category
-              key={category.id}
-              title={category.title}
-              text={category.text}
-            />
+            <>
+              <div className="category">
+                <a href={`/products?${category.id}`} className="aCategories">
+                  <h3>{category.title}</h3>
+                  <p>{category.text}</p>
+                </a>
+                <button
+                  type="button"
+                  onClick={() => {
+                    deleteCategory(category.id);
+                  }}
+                >
+                  Delete Category
+                </button>
+                <div className="edit-form">
+                  <input
+                    type="text"
+                    placeholder="title"
+                    onChange={(event) => {
+                      setEditTitleInput(event.target.value);
+                    }}
+                  />
+                  <input
+                    type="text"
+                    placeholder="text"
+                    onChange={(event) => {
+                      setEditTextInput(event.target.value);
+                    }}
+                  />
+                  <button
+                    type="button"
+                    id="button-form"
+                    className="form-opened"
+                    onClick={() => {
+                      editCategory(category.id);
+                    }}
+                  >
+                    Edit Category
+                  </button>
+                </div>
+              </div>
+            </>
           ))}
         </div>
       </div>
