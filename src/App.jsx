ed9a5cd9a32cfe2966d98c9863c38f8d3fe5c85e
Samuel Burla <samuelburla4@gmail.com>
2024-08-01 12:27:52 +0200

    Setup React-Router and first page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-// import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
 import "./App.css";
 
 function App() {
   return (
     <>
-      {/* <Routes>
+      <Routes>
         <Route path="/" element={<Home />} />
-      </Routes> */}
+        {/* <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} /> */}
+      </Routes>
     </>
   );
 }
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+
+const Home = () => {
+
+  return (
+    <>
+      <h1>hello world!</h1>
+    </>
+  );
+};
+
+export default Home;
